fcf29f8e85fffdbb0a910f7ebba830b858c2922c
RohitWadh <44647861+RohitWadh@users.noreply.github.com>
2019-06-01 17:44:05 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/open?id=1g4Dv2dDfAxtNXslZeRjGlKc0BLCmQ5Db'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1QfJW6YzC-WuVkRnMlWHqSX1KDlIHHPMv'
 export_file_name = 'stage-2.pkl'
 
 classes = ['cbb', 'cbsd', 'cgm', 'cmd', 'healthy']
